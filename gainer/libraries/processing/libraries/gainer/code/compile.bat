7b702800ec2b50111de909994d0d0147a0befb19
en_tk_m <en_tk_m@986d96f0-d712-0410-9fea-fd1e570f26e0>
2007-10-21 00:54:20 +0000

    git-svn-id: https://svn.code.sf.net/p/gainer/code/trunk@249 986d96f0-d712-0410-9fea-fd1e570f26e0

--- gainer/libraries/processing/libraries/gainer/code/compile.bat
+++
@@ -1,5 +0,0 @@
-SET PROCESSING=C:\processing-0115\
-SET PROCESSINGLIBS=C:\processing-0115\libraries\
-
-javac -target 1.3 -source 1.3 -classpath %PROCESSING%lib\core.jar;%PROCESSINGLIBS%serial\library\RXTXcomm.jar -d %PROCESSINGLIBS%gainer\code\ Digital.java Analog.java Gainer.java
-echo -Xlint:unchecked 
--- gainer/libraries/processing/libraries/gainer/code/compress.bat
+++
@@ -1,3 +0,0 @@
-SET PROCESSING =C:\processing-0115\
-
-jar cvfm %PROCESSING%libraries\gainer\library\gainer.jar MANIFEST.MF processing
